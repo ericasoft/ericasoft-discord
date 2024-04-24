a915517bc203fabb493e313a72f29e38ede2cf4c
ericasoft <dev-ericasoft@outlook.com>
2024-04-24 20:56:11 +0200

    [ESDISC-5] Initial implementation of repository layer for voice channel configurations

---
+++ ericasoft-discord-vchelper/src/main/java/org/ericasoft/discord/vchelper/VoiceChannelRepository.java
@@ -0,0 +1,15 @@
+package org.ericasoft.discord.vchelper;
+
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface VoiceChannelRepository extends CrudRepository<VoiceChannelConfiguration, Long> {
+
+    <S extends VoiceChannelConfiguration> S save(@NotNull S vcConfig);
+
+    @Override
+    Optional<VoiceChannelConfiguration> findById(Long aLong);
+}
